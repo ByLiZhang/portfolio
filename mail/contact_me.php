285e7fdbd1fa3e3d3c0df79870289a2269b9210a
ByLiZhang <32698353+ByLiZhang@users.noreply.github.com>
2018-05-04 23:49:52 -0700

    Added comments for new edits

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+//added headers for COR error
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
